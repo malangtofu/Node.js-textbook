23131c914d152a593192e035e5a3daf1dc11f168
js l <malangtofu507@gmail.com>
2023-12-11 21:54:09 +0900

    2023-09-15~2023-12-11까지 기록

---
+++ ch08/learn-mongoose/schemas/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+    if (process.env.NODE_ENV !== "production") {
+        mongoose.set("debug", true);
+    }
+
+    mongoose.connect("mongodb://root:mongo1103@localhost:27017/admin", {
+        dbName: "nodejs",
+        useNewUrlParser: true,
+    }).then(() => {
+        console.log("몽고디비 연결 성공");
+    }).catch((err) => {
+        console.log("몽고디비 연결 에러", err);
+    })
+};
+
+mongoose.connection.on("error", (error) => {
+    console.log("몽고디비 연결 에러", error);
+})
+mongoose.connection.on("disconnected", () => {
+    console.log("몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.");
+    connect();
+})
+
+module.exports = connect;
