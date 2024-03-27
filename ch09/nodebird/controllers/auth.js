12f1036cfdc482ddf5b59f7f236637f01a753aba
js l <malangtofu507@gmail.com>
2024-03-27 22:27:10 +0900

    2023-09-15~2024-03-27까지 기록

---
+++ ch09/nodebird/controllers/auth.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const User = require('../models/user');
+
+exports.join = async (req, res, next) => {
+    const { email, nick, password } = req.body;
+    try {
+        const exUser = await User.findOne({ where: { email }});
+        if (exUser) {
+            return res.redirext('/join?error=exist');
+        }
+        const hash = await bcrypt.hash(password, 12);
+        await User.create({
+            email,
+            nick,
+            password: hash,
+        });
+        return res.redirect('/');
+    } catch(error) {
+        console.error(error);
+        return next(error);
+    };
+}
+
+// POST auth/login
+exports.login = (req, res, next) => {};
+
+exports.logout = (req, res) => {};
